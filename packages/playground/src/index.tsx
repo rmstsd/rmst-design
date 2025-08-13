435ddd85b5fa75da49b9a9b2f215a3ab30df0dcf
rmstsd <rmstsd@qq.com>
2025-08-13 14:23:29 +0800

    up

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 
 import './index.less'
 
@@ -9,8 +9,8 @@ const rootEl = document.getElementById('root')
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl)
   root.render(
-    <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   )
 }
