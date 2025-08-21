8dbcea9cc024b7adc945ff961d19d381738eb97d
rmstsd <rmstsd@qq.com>
2025-08-21 16:08:51 +0800

    up

--- packages/playground/src/pages/AHome/AHome.tsx
+++ packages/playground/src/pages/AHome/AHome.tsx
@@ -1,49 +1,111 @@
 import axios from 'axios'
 import { Suspense, use, useState } from 'react'
+import { sleep } from '../../utils'
 
-const api = ({ delay = 500, data = 'mockData' } = {}) => {
-  return axios.post(`http://localhost:1400/test`, { delay, data }).then(res => res.data)
+const api = ({ delay = 500, name = 'mockData' } = {}): Promise<{ data: any }> => {
+  return axios.post(`http://localhost:1400/test`, { delay, name }).then(res => res.data)
 }
 
-export default function AHome() {
-  console.log(1)
+const count = 3
+const url = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`
 
-  let [p, setP] = useState(() => api({ delay: 1000 }))
+const getUserList = async ({ delay = 500 } = {}): Promise<{ data: any }> => {
+  const abct = new AbortController()
+
+  await sleep(delay)
+
+  const p = axios.get(url, { signal: abct.signal }).then(res => res.data.results)
+
+  p.cancel = () => {
+    abct.abort()
+  }
+
+  return p
+}
+
+const oo = getUserList()
+
+console.log(oo)
+
+export default function AHome() {
+  const [input, setInput] = useState('')
+
+  let [p, setP] = useState(() => {
+    return [api()]
+  })
 
   return (
     <div>
       <button
         onClick={() => {
-          setP(api({ delay: 500, data: '1' }))
+          setP([api({ delay: 1000 }), api({ delay: 2000 })])
         }}
       >
-        setP 1秒
-      </button>
-      <button
-        onClick={() => {
-          setP(api({ delay: 2000, data: '2' }))
-        }}
-      >
-        setP 2秒
+        set P
       </button>
 
-      <Suspense fallback={<div>loading</div>}>
-        <Child p={p} />
+      <input
+        type="text"
+        value={input}
+        onChange={evt => {
+          const val = evt.target.value
+          setInput(val)
+
+          console.log(p)
+          // p.forEach(item => {
+          //   item?.cancel()
+          // })
+
+          setP([getUserList()])
+        }}
+      />
+
+      <Suspense fallback={<div className="bg-pink-100 p-2">loading </div>}>
+        <Child p={p[0]} />
+        <Child2 p={p[1]} />
       </Suspense>
 
-      <Child2 p={p} />
+      {/* {p.map((item, index) => (
+        
+      ))} */}
     </div>
   )
 }
 
 const Child = props => {
-  console.log('c')
-  let data = use(props.p)
-  console.log(data)
+  const { p } = props
 
-  return <div>{JSON.stringify(data)}</div>
+  if (!p) {
+    return null
+  }
+
+  let data = use(p)
+
+  return (
+    <ul className="border bg-purple-50">
+      {data.map(item => (
+        <li key={item.name}>{item.name}</li>
+      ))}
+    </ul>
+  )
 }
 
-const Child2 = props => {
-  return <div>Child2</div>
+const Child2 = ({ p }) => {
+  return (
+    <Suspense fallback={<div className="bg-pink-100 p-2">loading Child2</div>}>
+      <Child3 p={p} />
+    </Suspense>
+  )
+}
+
+const Child3 = ({ p }) => {
+  let data = use(p)
+
+  return (
+    <ul className="border bg-purple-50">
+      {data.map(item => (
+        <li key={item.name}>{item.name}</li>
+      ))}
+    </ul>
+  )
 }
---
+++ packages/playground/src/utils/index.ts
@@ -0,0 +1,6 @@
+// sleep
+export const sleep = (delay = 500) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, delay)
+  })
+}
