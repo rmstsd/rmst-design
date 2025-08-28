12e6bf0a066dcb7ca1566d2547efe8247485422f
rmstsd <rmstsd@qq.com>
2025-08-28 21:47:55 +0800

    up

--- packages/playground-nextjs/src/app/blog/page.tsx
+++ packages/playground-nextjs/src/app/blog/page.tsx
@@ -4,17 +4,17 @@ import Client from './Client'
 export default function Blog() {
   console.log('test render')
 
-  const p = fetch('http://localhost:1400/test', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ delay: 10, name: '接口' })
-  }).then(res => res.json())
+  // const p = fetch('http://localhost:1400/test', {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ delay: 10, name: '接口' })
+  // }).then(res => res.json())
 
   return (
     <div className="">
       <div>blog</div>
 
-      <Client p={p} />
+      {/* <Client p={p} /> */}
 
       <Modal>
         <div>Modal content</div>
