f81b14375251489427201908c23b9ac0528953b4
rmstsd <rmstsd@qq.com>
2025-09-16 17:22:14 +0800

    dd

--- packages/playground-nextjs/src/app/page.tsx
+++ packages/playground-nextjs/src/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image'
 
+import lyy from '@/assets/lyy.jpg'
+
 export default async function Home() {
-  console.log(fetch)
-
-  console.log(async function Aa() {})
-
   // 随机数据
   const res = await fetch('https://api.github.com/users/rmstsd')
   const data = await res.json()
@@ -14,6 +12,12 @@ export default async function Home() {
       <center>Home</center>
 
       <Image src={data.avatar_url} width={100} height={100} alt="" />
+
+      <Image src={lyy} alt="" style={{ objectFit: 'cover', width: 100, height: 100 }} />
+
+      <div className="relative" style={{ width: 400, aspectRatio: 1.5 }}>
+        <Image src="/lyy.jpg" alt="" fill style={{ objectFit: 'cover' }} decoding="async" />
+      </div>
 
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
