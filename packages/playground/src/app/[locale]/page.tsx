d82bae2a9d6e044668e3c139fbf43a1700e55c4b
rmstsd <rmstsd@qq.com>
2025-11-04 10:44:54 +0800

    up

--- packages/playground/src/app/[locale]/page.tsx
+++ packages/playground/src/app/[locale]/page.tsx
@@ -1,40 +1,45 @@
 'use client'
 
-import { useState } from 'react'
+import { use, useLayoutEffect, useRef, useState } from 'react'
 import { Button } from 'rmst-design'
 
 // vue 官网动画怎么实现的 https://cn.vuejs.org/guide/built-ins/transition-group.html#move-transitions
 export default function Home() {
   const [visible, setVisible] = useState(false)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    if (!visible) {
+      return
+    }
+
+    const el = ref.current
+    let height = el.clientHeight
+    el.style.height = 0
+    el.style.transform = 'scale(0.2)'
+    el.style.overflow = 'hidden'
+
+    requestAnimationFrame(() => {
+      el.style.transition = 'height 1s, transform 0.5s'
+      el.style.height = `${height}px`
+      el.style.transform = 'scale(1)'
+    })
+  }, [visible])
 
   return (
     <div>
       <Button onClick={() => setVisible(!visible)}>Toggle</Button>
 
-      {visible && (
-        <div
-          className=" bg-red-100 transition-all h-11"
-          ref={el => {
-            if (el) {
-              let height = el.clientHeight
-              el.style.height = 0
-              el.style.overflow = 'hidden'
+      <div className="flex flex-col gap-2 p-2">
+        {visible && (
+          <div className=" bg-red-100 " style={{ height: 60 }} ref={ref}>
+            Input
+          </div>
+        )}
 
-              el.style.transform = 'scale(0.5)'
-
-              setTimeout(() => {
-                el.style.height = `${height}px`
-                el.style.transform = 'scale(1)'
-              }, 16)
-            }
-          }}
-        >
-          Input
-        </div>
-      )}
-
-      <div className="p-3 bg-pink-100">Select</div>
-      <div className="p-3 bg-orange-100">DatePicker</div>
+        <div className="p-3 bg-pink-100">Select</div>
+        <div className="p-3 bg-orange-100">DatePicker</div>
+      </div>
     </div>
   )
 }
