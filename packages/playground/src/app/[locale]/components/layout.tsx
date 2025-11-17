9a41fb0ae0e8a3922ceaf0f951752696246b93f5
rmstsd <rmstsd@qq.com>
2025-11-17 21:38:51 +0800

    up

--- packages/playground/src/app/[locale]/components/layout.tsx
+++ packages/playground/src/app/[locale]/components/layout.tsx
@@ -4,11 +4,11 @@ import { PropsWithChildren } from 'react'
 export default function Layout(props: PropsWithChildren) {
   return (
     <div className="flex">
-      <div className="w-[160px] shrink-0">
+      <div className="w-[160px] shrink-0 fixed top-[48px] left-0 bottom-0">
         <Side />
       </div>
 
-      <section className="p-2 flex-grow">{props.children}</section>
+      <section className="p-2 flex-grow ml-[160px]">{props.children}</section>
     </div>
   )
 }
