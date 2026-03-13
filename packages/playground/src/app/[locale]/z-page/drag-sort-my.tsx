070c4c4f2c797feeae14d3653745229ed0fed005
rmstsd <rmstsd@gmail.com>
2026-03-13 18:04:22 +0800

    v2

--- packages/playground/src/app/[locale]/z-page/drag-sort-my.tsx
+++ packages/playground/src/app/[locale]/z-page/drag-sort-my.tsx
@@ -13,7 +13,13 @@ export const DragSortMy = observer(() => {
       items: initialItems,
       activeIndex: null,
       overIndex: null,
-      translateY: []
+      translateY: [],
+
+      dragState: {
+        down_clientY: 0,
+        down_scrollTop: 0,
+        move_clientY: 0
+      }
     }
   })
 
@@ -25,53 +31,30 @@ export const DragSortMy = observer(() => {
   }
 
   const handlePointerDown = async (downEvt: PointerEvent, id: string, index: number) => {
-    calcPosition()
-
-    const down_clientY = downEvt.clientY
+    state.dragState.down_clientY = downEvt.clientY
+    state.dragState.move_clientY = downEvt.clientY
     state.activeIndex = index
 
     const container = document.getElementById('rmst-container')
-    let down_scrollTop = container.scrollTop
+    const containerRect = container.getBoundingClientRect()
+    state.dragState.down_scrollTop = container.scrollTop
+
+    calcListPosition()
+
+    autoScroll()
 
     startDrag(downEvt, {
       onDragMove: moveEvent => {
         let move_clientY = moveEvent.clientY
+        state.dragState.move_clientY = move_clientY
 
-        const dy = container.scrollTop - down_scrollTop
-        move_clientY += dy
-
-        const rects = Array.from(domsMapRef.current).map(([id, rect]) => ({ id, rect }))
-        // 寻找到最近的一个元素
-        const overIndex = rects.reduce(
-          (closest, item, i) => {
-            const center = item.rect.top + item.rect.height / 2
-            const distance = Math.abs(move_clientY - center)
-            return distance < closest.distance ? { index: i, distance } : closest
-          },
-          { index: -1, distance: Infinity }
-        ).index
-
-        state.overIndex = overIndex
-        const { activeIndex } = state
-
-        const translateY = []
-        if (overIndex > activeIndex) {
-          const offset = rects[activeIndex].rect.top - rects[activeIndex + 1].rect.top
-
-          for (let i = activeIndex + 1; i <= overIndex; i++) {
-            translateY[i] = offset
-          }
-        } else if (overIndex < activeIndex) {
-          const offset = rects[activeIndex].rect.bottom - rects[activeIndex - 1].rect.bottom
-
-          for (let i = activeIndex - 1; i >= overIndex; i--) {
-            translateY[i] = offset
-          }
-        } else if (overIndex === activeIndex) {
+        if (containerRect.top <= move_clientY && move_clientY <= containerRect.bottom) {
+          speedRef.current = 0
+        } else if (move_clientY < containerRect.top) {
+          speedRef.current = move_clientY - containerRect.top
+        } else if (containerRect.bottom < move_clientY) {
+          speedRef.current = move_clientY - containerRect.bottom
         }
-
-        translateY[activeIndex] = move_clientY - down_clientY
-        state.translateY = [...translateY]
       },
       onDragEnd: () => {
         if (state.overIndex !== state.activeIndex) {
@@ -88,19 +71,80 @@ export const DragSortMy = observer(() => {
       state.activeIndex = null
       state.overIndex = null
       state.translateY = []
+
+      speedRef.current = 0
+
+      cancelAnimationFrame(rafId.current)
     }
   }
 
-  const calcPosition = () => {
+  const calcListPosition = () => {
     for (const [id, el] of domRefs.current) {
       const rect = el.getBoundingClientRect().toJSON()
       domsMapRef.current.set(id, rect)
     }
   }
 
+  const calcTranslateY = () => {
+    let { down_clientY, down_scrollTop, move_clientY } = state.dragState
+
+    const container = containerRef.current
+    const dy = container.scrollTop - down_scrollTop
+    move_clientY += dy
+
+    const rects = Array.from(domsMapRef.current).map(([id, rect]) => ({ id, rect }))
+    // 寻找到最近的一个元素
+    const overIndex = rects.reduce(
+      (closest, item, i) => {
+        const center = item.rect.top + item.rect.height / 2
+        const distance = Math.abs(move_clientY - center)
+        return distance < closest.distance ? { index: i, distance } : closest
+      },
+      { index: -1, distance: Infinity }
+    ).index
+
+    state.overIndex = overIndex
+    const { activeIndex } = state
+
+    const translateY = []
+    if (overIndex > activeIndex) {
+      const offset = rects[activeIndex].rect.top - rects[activeIndex + 1].rect.top
+
+      for (let i = activeIndex + 1; i <= overIndex; i++) {
+        translateY[i] = offset
+      }
+    } else if (overIndex < activeIndex) {
+      const offset = rects[activeIndex].rect.bottom - rects[activeIndex - 1].rect.bottom
+
+      for (let i = activeIndex - 1; i >= overIndex; i--) {
+        translateY[i] = offset
+      }
+    } else if (overIndex === activeIndex) {
+    }
+
+    translateY[activeIndex] = move_clientY - down_clientY
+    state.translateY = [...translateY]
+  }
+
+  let speedRef = useRef(0)
+  const rafId = useRef<number>(0)
+
+  function autoScroll() {
+    if (speedRef.current !== 0) {
+      const rmstContainer = document.querySelector('#rmst-container')
+      rmstContainer.scrollBy(0, speedRef.current)
+    }
+
+    calcTranslateY()
+
+    rafId.current = requestAnimationFrame(autoScroll)
+  }
+
+  const containerRef = useRef<HTMLDivElement>(null)
+
   return (
     <div className="rmstsd-dsm-c relative mt-10">
-      <div id="rmst-container" className="px-4 space-y-2 overflow-auto" style={{ height: 600 }}>
+      <div id="rmst-container" ref={containerRef} className="px-4 space-y-2 overflow-auto" style={{ height: 600 }}>
         {state.items.map((item, index) => {
           const isActive = state.activeIndex === index
 
